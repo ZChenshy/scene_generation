65a8bb0f3d4f2374c3a826dfa66315ff5d2cd3ac
chipaang <chipaang@qq.com>
2024-01-03 14:51:30 +0000

    remove sdxl file

--- threestudio/models/guidance/__init__.py
+++ threestudio/models/guidance/__init__.py
@@ -2,5 +2,4 @@ from . import (
     deep_floyd_guidance,
     stable_diffusion_guidance,
     depth_controlnet_SD15_guidance,
-
 )
--- threestudio/models/prompt_processors/__init__.py
+++ threestudio/models/prompt_processors/__init__.py
@@ -1,6 +1,5 @@
 from . import (
     base,
-    sdxl_base,
     deepfloyd_prompt_processor,
     stable_diffusion_prompt_processor,
 )
