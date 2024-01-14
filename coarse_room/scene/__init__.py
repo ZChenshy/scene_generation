9392acceefee19a0f24f3879f579c2762b021196
zcs <1790328901@qq.com>
2024-01-14 09:37:29 +0000

    modidy pcd load

--- coarse_room/scene/__init__.py
+++ coarse_room/scene/__init__.py
@@ -12,6 +12,7 @@
 import os
 import random
 import json
+import trimesh
 from utils.system_utils import searchForMaxIteration
 from utils.graphics_utils import BasicPointCloud
 from scene.dataset_readers import sceneLoadTypeCallbacks
@@ -54,6 +55,21 @@ class Scene:
                 np.array(pcd.colors),
                 np.repeat(np.array([0,0,0]),np.array(pcd.colors).shape[0],axis=0)
             )
+            if os.path.exists(os.path.join(os.path.join(args.source_path,'scene_pcd.ply'))):
+                pcd = trimesh.load(os.path.join(args.source_path,'scene_pcd.ply'))
+                #pcd.vertices =pcd.vertices * np.array([1, 1, 1])
+                pcd = normal_scene(pcd)
+                pcd.show()
+                ###这里为测试时使用的代码，后续需要删除
+                #在点云数据中加入坐标轴，
+                #pcd = add_axis(1,1,1, pcd)
+                # pcd.show()
+                ##############################
+                scene_info = BasicPointCloud(
+                    np.array(pcd.vertices),
+                    np.array(pcd.colors[:, :3]),
+                    np.repeat(np.array([0,0,0]),np.array(pcd.colors).shape[0],axis=0)
+                )
         else:
             print(f"{args.model_path}/scene_pcd.ply is not exists")
         
@@ -75,3 +91,13 @@ class Scene:
 
     def getTestCameras(self, scale=1.0):
         return self.test_cameras[scale]
+    
+    def normal_scene(scene):
+        points = scene.vertices
+        scene_bbox = scene.bounds
+        scene_centroid = scene.centroid
+        scene.vertices = scene.vertices - scene_centroid
+        scale_factor = 1 / (scene_bbox[1][1] - scene_bbox[0][0])
+        scene.vertices = scene.vertices  * np.array([scale_factor, scale_factor, scale_factor])
+        
+        return scene
